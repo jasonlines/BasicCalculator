a4faa316f6ac1883b1d95f637059b3a93002e902
Jason Lines <j.lines@uea.ac.uk>
2023-05-12 11:13:51 +0100

    Broken change to addition

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,7 +3,7 @@ package org.example;
 public class Calculator {
 
     public static int addition(int x, int y){
-        return x + y;
+        return x * y;
     }
 
     public static int subtraction(int x, int y){
